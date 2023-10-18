6828b0deef76ed594040e9c734b5d52723cc5fb6
KirisameKei <53395204+KirisameKei@users.noreply.github.com>
2023-10-18 17:02:48 +0900

    [FIX] 日付が間違っていたのを修正

--- limited_time.py
+++ limited_time.py
@@ -26,7 +26,7 @@ async def simple_kikaku_join(message):
         return
 
     now = datetime.datetime.now()
-    finish_time = datetime.datetime(2023, 11, 19, 12, 0)
+    finish_time = datetime.datetime(2023, 11, 18, 12, 0)
     if now >= finish_time:
         await message.channel.send("現在企画は行われていません")
         return
