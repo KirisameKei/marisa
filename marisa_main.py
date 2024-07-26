ee9cfeb205b58acdf3f8e4e18e68de0028e17984
KirisameKei <53395204+KirisameKei@users.noreply.github.com>
2024-07-27 06:23:44 +0900

    [本番] 整地大会結果発表デバッグ終了

--- marisa_main.py
+++ marisa_main.py
@@ -566,7 +566,7 @@ async def kikaku_announcement():
         await client1.wait_until_ready()
         now = datetime.datetime.now()
 
-        if now.month == 7 and now.day == 27 and now.hour == 0 and now.minute == 10:
+        if now.month == 8 and now.day == 19 and now.hour == 0 and now.minute == 10:
 #        if now.month == 8 and now.day == 19 and now.hour == 0 and now.minute == 0: #本番
             #await limited_time.simple_kikaku_result(client1) #応募者の中からn人選ぶシンプルな企画
             #await limited_time.complex_kikaku_result(client1) #総額いくらを当選人数人でランダムに分配する企画
