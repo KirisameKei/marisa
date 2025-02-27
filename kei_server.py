10af5fba406b02271aa8c166236b29b73cad364a
KirisameKei <53395204+KirisameKei@users.noreply.github.com>
2025-02-27 21:17:17 +0900

    [FIX] ログボの魔理沙排除機能において調べられる限り全ての空白文字に対応

--- kei_server.py
+++ kei_server.py
@@ -961,9 +961,20 @@ async def login_bonus(message):
     msg = jaconv.z2h(msg, ignore="", kana=False, ascii=True, digit=True) #英数のみ半角にする
     msg = jaconv.kata2hira(msg, ignore="") #全てひらがなにする
     msg = msg.lower() #小文字にする
-    msg = msg.replace(" ", "").replace("\n", "").replace("゛", "").replace("​", "").replace("っ", "").replace("-", "").replace("ー", "") #邪魔な装飾を消す(全角スペースはz2hで消えてる)
+    space_tuple = (
+        '\u0009', '\u0020', '\u00A0', '\u00AD', '\u034F', '\u061C', '\u115F', '\u1160', '\u17B4', '\u17B5',
+        '\u180E', '\u2000', '\u2001', '\u2002', '\u2003', '\u2004', '\u2005', '\u2006', '\u2007', '\u2008',
+        '\u2009', '\u200A', '\u200B', '\u200C', '\u200D', '\u200E', '\u200F', '\u202F', '\u205F', '\u2060',
+        '\u2061', '\u2062', '\u2063', '\u2064', '\u206A', '\u206B', '\u206C', '\u206D', '\u206E', '\u206F',
+        '\u3000', '\u2800', '\u3164', '\uFEFF', '\uFFA0', '\u1D159', '\u1D173', '\u1D174', '\u1D175', '\u1D176',
+        '\u1D177', '\u1D178', '\u1D179', '\u1D17A'
+    )
+    for space in space_tuple:
+        msg = msg.replace(space, "")
+    msg = msg.replace("\n", "").replace("゛", "").replace("っ", "").replace("-", "").replace("ー", "") #邪魔な装飾を消す(全角スペースはz2hで消えてる)
     msg = msg.replace("chan", "ちゃん").replace("tyan", "ちゃん").replace("tan", "たん") #英語でのちゃん付けを変換
     msg = msg.replace("ma", "ま").replace("ri", "り").replace("sa", "さ") #ローマ字をひらがなに変換
+    print(msg)
     #この時点で全角ひらがなと半角英数のみ
     NG_word_tuple = (
         "魔理",
