9295b55c0ca5f025394de2f9405abba78837a4b2
KirisameKei <53395204+KirisameKei@users.noreply.github.com>
2025-11-04 13:12:56 +0900

    [CHANGE] 7周年企画用にコードを変更

--- limited_time.py
+++ limited_time.py
@@ -210,7 +210,7 @@ async def simple_kikaku_result(client1):
     kikaku_role = guild.get_role(668021019700756490)
     
     try:
-        tousen = random.sample(kikaku_role.members, k=3) #kは当選人数
+        tousen = random.sample(kikaku_role.members, k=5) #kは当選人数
     except ValueError:
         tousen = kikaku_role.members
 
@@ -238,11 +238,12 @@ async def simple_kikaku_result(client1):
     ch = client1.get_channel(586420858512343050)
     await ch.send(content="<@&668021019700756490>", embed=embed)
     await ch.send(
-        "**受け取り期日は2025/1/31までとします\n"
-        "**当選者で事情により期限内に受け取れない場合は期限内に言っていただければ対応します。\n"
-        "kirisamekei都合で受け渡しができない可能性があります。その際は受け取り期限を延長/廃止します。\n"
-#        "参加賞はmineにてお渡しします。受け取り希望の方は<#597130965927723048>にそれとわかるメッセージを送信してください。\n"
-#        "受け取り辞退をする場合<#665487669953953804>にて`/cancel`をしてください。"
+#        "**受け取り期日は2025/1/31までとします\n"
+        "**受け取り期日は鯖復活後告知します**\n"
+#        "**当選者で事情により期限内に受け取れない場合は期限内に言っていただければ対応します。\n"
+#        "kirisamekei都合で受け渡しができない可能性があります。その際は受け取り期限を延長/廃止します。\n"
+        "参加賞を受け取り希望の方は<#597130965927723048>にそれとわかるメッセージを送信してください。\n"
+        "受け取り辞退をする場合<#665487669953953804>にて`/cancel`をしてください。"
     )
 
 
